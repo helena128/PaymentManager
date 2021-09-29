4c9247319c22c2ff10684400bfa212fb03bd5aab
helena128 <hellogbel@gmail.com>
2021-09-29 12:37:33 +0300

    Initial commit

---
+++ src/test/java/com/helena128/paymentmanager/PaymentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helena128.paymentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
